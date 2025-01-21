e36ec00961d9afa0fd45833ac99475d910e84616
Buddy1403 <marvreyes123@gmail.com>
2025-01-21 12:28:51 +0800

    Summary adjustments v3

--- app/Http/Controllers/SeasonController.php
+++ app/Http/Controllers/SeasonController.php
@@ -2,22 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Season;
-use App\Http\Responses\ApiSuccessResponse;
 use App\Http\Requests\Season\SeasonRequest;
+use App\Http\Responses\ApiSuccessResponse;
+use App\Models\Season;
 use Symfony\Component\HttpFoundation\Response;
 
 class SeasonController extends Controller
-{      
+{
     public function countRegistry(SeasonRequest $request)
     {
         $validated = $request->validated();
 
-        $season = Season::where('year', $validated['year']);
+        $season = Season::where('created_by', auth()->user()->id)->where('year', $validated['year']);
 
         $data = [
             'data' => $season->get(),
-            'total_entry' => $season->sum('entry')
+            'total_entry' => $season->sum('entry'),
         ];
 
         return new ApiSuccessResponse(
