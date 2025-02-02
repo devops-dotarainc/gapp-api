4efdc01c05303efbe1a81f963f68fb38a527cdc9
eduvanekyle <eduvanekyle@gmail.com>
2025-02-02 23:40:43 +0800

    Added laravel pulse config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Laravel\Pulse\Facades\Pulse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', function (?User $user) {
+            return true;
+        });
+
+        Pulse::user(fn($user) => [
+            'name' => $user->first_name . ' ' . $user->last_name,
+            'extra' => $user->username,
+        ]);
     }
 }
